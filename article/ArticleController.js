c68ca38c1fa6798888bea994af5f0693e41f25b9
KaykeAmorim <kaykeamorim6@gmail.com>
2022-06-13 22:51:31 -0300

    Artigos INDEX

--- article/ArticleController.js
+++ article/ArticleController.js
@@ -1,9 +1,32 @@
 const express = require('express');
-const req = require('express/lib/request');
+const Category = require('../categories/Category')
 const router = express.Router();
+const Article = require('./Article')
+const slugify = require('slugify')
 
-router.get('/article', (req,res) =>{
-    res.send("Eae CBA ?")
+router.get('/adm/articles', (req,res) =>{
+    res.render('adm/articles/index',{articlesInfo: []})
+})
+
+router.get('/adm/articles/new', (req, res) => {
+    Category.findAll({raw:true}).then((result) => {
+        res.render('adm/articles/new', {categories: result})
+    })
+})
+
+router.post('/articles/save', (req,res) => {
+    let title = req.body.title
+    let body = req.body.body
+    let category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect('/adm/')
+    })
 })
 
 module.exports = router
